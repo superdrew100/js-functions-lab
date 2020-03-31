abe12c5c36cd367d3749380c07f3b22372358ec9
Drew Thomasson <superdrew100@Gmail.com>
2020-03-31 11:52:44 -0400

    worked on it some more

---
+++ script.js
@@ -0,0 +1,35 @@
+//1. If you have a code that you need to reuse so you can have a dry code
+//2.
+
+
+//write a function definition without parameters
+
+
+function reverseString(string){
+    let stepCount = string.length
+    for(let i + 0; stepCount; i++)
+
+}
+
+reverseString("four"){
+
+}
+
+
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
+
+//2
+function numberSquared(num){
+    return num * num
+}
+numberSquared(3)
+
+console.log(numberSquared(3))
