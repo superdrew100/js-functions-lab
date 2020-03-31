f0cb87eac3988566cc8358c1ceec4b50bb5ae93d
Drew Thomasson <superdrew100@Gmail.com>
2020-03-31 14:44:54 -0400

    more ntos druing classwork tryign tot think through it

--- script.js
+++ script.js
@@ -33,3 +33,30 @@ function numberSquared(num){
 numberSquared(3)
 
 console.log(numberSquared(3))
+
+
+
+cuntion adder(num1 = 0, num2 = 0, num3 = 0) {
+    if (num1 === undefined && 
+        num2 === undefined &&
+        num3 === undefined) {
+            return 10
+        }
+        if(num1 = undefined) {
+            num1 = 0
+        }
+    num1= num1 || 0
+    num2= num2 || 0
+    num3= num3 || 0
+
+    return num1 + num2 + num3 
+}
+const answer1 = adder(1, 2, 4)
+console.log('adder answer' , answer)
+const answer2 = adder()
+console.log('adder answer' , answer)
+const answer3 = adder(1, 2,)
+console.log('adder answer' , answer)
+
+
+//
